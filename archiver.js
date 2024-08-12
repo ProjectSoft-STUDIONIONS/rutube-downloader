dc7a4c30bfd07f3eea975f30fb7593c482fc9c02
ProjectSoft <projectsoft2009@yandex.ru>
2024-08-12 15:07:47 +0400

    add ffmpeg converting

--- archiver.js
+++ archiver.js
@@ -8,6 +8,34 @@ const fs = require('node:fs'),
 
 (async function(){
 	console.log(" ");
+	let jsn = fs.readFileSync(path.normalize(path.join(__dirname, "package.json")));
+
+	let json = JSON.parse(jsn);
+
+	let oldPackage = {
+		"name": json.name,
+		"description": json.description,
+		"version": json.version,
+		"main": json.main,
+		"scripts": json.scripts,
+		"author": json.author,
+		"license": json.license,
+		"dependencies": {
+			"ansi-colors": "4.1.3",
+			"cli-progress": "3.12.0",
+			"m3u8-parser": "7.1.0",
+			"node-fetch": "2.6.1",
+			"sanitize-filename": "1.6.3",
+			"split-file": "2.3.0"
+		}
+	};
+	
+	let str = JSON.stringify(oldPackage, null, "\t");
+
+	fs.writeFileSync(path.normalize(path.join(__dirname, "package.json")), str);
+	console.log("SAVE:", "package.json");
+	await delay(500);
+	console.log(" ");
 	const output = fs.createWriteStream(__dirname + '/rutube-downloader.zip'),
 		archive = archiver('zip', {
 			zlib: { level: 9 }
--- index.js
+++ index.js
@@ -16,6 +16,8 @@ const path = require('node:path');
 const URL = require('node:url');
 const stream = require('node:stream');
 const util = require('node:util');
+const { execFileSync, execSync, spawnSync } = require('node:child_process');
+
 // Установленные
 const fetch = require('node-fetch');
 const m3u8Parser = require('m3u8-parser');
@@ -23,6 +25,7 @@ const splitFile = require('split-file');
 const cliProgress = require('cli-progress');
 const _colors = require('ansi-colors');
 const sanitize = require('sanitize-filename');
+const ffmpeg = require('ffmpeg');
 
 const regex_rutube = /^https?:\/\/rutube\.ru\/video\/(\w+)/;
 
@@ -56,6 +59,21 @@ const escapeRegExp = function(text) {
 				fs.unlinkSync(dir + f)
 			});
 			resolve(true);
+		})
+	},
+
+	deleteFile = async function(file) {
+		return new Promise((resolve, reject) => {
+			fs.stat(file, function(err, stat) {
+				if (err == null) {
+					fs.unlinkSync(file);
+					resolve(true);
+				} else if (err.code === 'ENOENT') {
+					resolve(true);
+				} else {
+					reject(false);
+				}
+			});
 		})
 	},
 
@@ -98,114 +116,141 @@ const escapeRegExp = function(text) {
 			ch = ' ';
 		return str.padEnd(len, ch);
 	},
-	delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-	
-if (urls.length) {
-	url = urls[0];
-	// Если нашли id ролика
-	if ((m = regex_rutube.exec(url)) !== null) {
-		// Получаем информацию о видео
-		pls = `https://rutube.ru/api/play/options/${m[1]}/?no_404=true&referer=https%3A%2F%2Frutube.ru`;
-		fetch(pls)
-			.then(res => res.json())
-			.then((json) => {
-				// Получаем название ролика
-				const outputTitle = json.title;
-				// Получаем информацию о плейлистах
-				let video_m3u8 = json['video_balancer']['m3u8'];
-				fetch(video_m3u8)
-					.then(res => res.text())
-					.then(text => {
-						let parser = new m3u8Parser.Parser();
-						parser.push(text);
-						parser.end();
-						let parsedManifest = parser.manifest;
-						// Получаем ссылку на плейлист самого большого разрешения видео
-						let m3u8 = parsedManifest['playlists'][parsedManifest['playlists'].length -1]['uri'];
-						// Получаем ссылку для составления в будующем ссылки на сегмент
-						const myURL = URL.parse(m3u8);
-						let pathname = myURL.pathname.split("/");
-						pathname.pop();
-						const urlPrefix = myURL.protocol + "//" + myURL.host + "/" + pathname.join("/") + "/";
-						// Получаем плейлист с сегментами
-						fetch(m3u8)
-							.then(res => res.text())
-							.then(async text => {
-								await createDir(__dirname + "/video");
-								await deleteFiles(/^segment-.*\.ts/, __dirname + '/video');
-								console.log(" ");
-								console.log("DOWNLOAD:", outputTitle, "\n");
+	delay = ms => new Promise(resolve => setTimeout(resolve, ms)),
 
-								const progress = new cliProgress.SingleBar({
-									stopOnComplete: true,
-									hideCursor: false,
-									autopadding: true,
-									fps: 5,
-									barsize: 37
-								},{
-									format: formatBar,
-									barCompleteChar: '\u2588',
-									barIncompleteChar: '\u2592'
-								});
-								let m3u8Video = new m3u8Parser.Parser();
-								m3u8Video.push(text);
-								m3u8Video.end();
-								// Сегменты
-								let segments = m3u8Video.manifest.segments;
-								let key, data = "", int;
-								// Получаем ссылки сегментов
-								let arrFiles = [];
-								// Забрать расширение сегмента
-								let ext;
-								progress.start(segments.length, 0, {filename: " "});
-
-								for (key in segments) {
-									// Забираем расширение
-									ext = path.extname(segments[key]['uri']);
-									// По сути здесь можно качать и сохранять
-									int = parseInt(key) + 1;
-									let fname = 'segment-' + `${int}`.padStart(10, '0') + ext;
-									try {
-										let rs = await fetch(urlPrefix + segments[key]['uri']);
-										if(rs.ok){
-											const f = __dirname + "/video/" + fname;
-											arrFiles.push(f);
-											progress.update(int, {filename: "   SAVE: " + _colors.yellowBright(fname)});
-											await streamPipeline(rs.body, fs.createWriteStream(f));
-										}else{
-											progress.update(int, {filename: "NO SAVE: " + _colors.redBright(fname)});
-											progress.stop();
-											console.error(_colors.redBright("ПРОВЕРЬТЕ ПОДКЛЮЧЕНИЕ К ИНТЕРНЕТУ"));
-											process.exit(1);
-										}
-									}catch(e){
-										progress.update(int, {filename: "NO SAVE: " + _colors.redBright(fname)});
-										progress.stop();
-										console.error(_colors.redBright("ПРОВЕРЬТЕ ПОДКЛЮЧЕНИЕ К ИНТЕРНЕТУ"));
-										process.exit(1);
-									}
-									await delay(50);
-								}
-								progress.update(int, {filename: " "});
-								await delay(1000);
-								progress.stop();
-								const saveTitle = sanitize(outputTitle);
-								console.log(" ");
-								console.log("COMBINING FILES:", _colors.yellowBright(`${arrFiles.length}`), "FILES INTO A", _colors.yellowBright(`"${saveTitle}${ext}"`), "PLEASE WAIT...", "\n");
-								await splitFile.mergeFiles(arrFiles, `${__dirname}/video/${saveTitle}${ext}`);
-								console.log(" ");
-								console.log("DELETE FILES:", _colors.yellowBright(`${arrFiles.length}`), "\n");
-								await deleteFiles(/^segment-.*\.ts/, __dirname + '/video');
-								console.log("DONE!", "\n");
-								console.log(_colors.cyanBright("Дальше надо делать обработку скаченного файла...))). Будем стараться. Пока всё."));
-								console.log(" ");
-								console.log(_colors.bgWhite(_colors.white("█████████████████")));
-								console.log(_colors.bgRed(_colors.white(" #СвоихНеБросаем ")));
-								console.log(_colors.bgBlue(_colors.blue("█████████████████")));
-								console.log(" ");
-							})
-					});
+	execFFmpeg = async function (input, output) {
+		return new Promise((resolve, reject) => {
+			const child = require('node:child_process')
+				.exec(`ffmpeg -i "${input}" "${output}"`);
+			child.stdout.pipe(process.stdout);
+			child.on('exit', () => {
+				resolve(true);
 			});
+		})
 	}
-}
+
+	runDownLoadAndConverting = async function(_url) {
+		return new Promise((resolve, reject) => {
+			if ((m = regex_rutube.exec(_url)) !== null) {
+				// Получаем информацию о видео
+				pls = `https://rutube.ru/api/play/options/${m[1]}/?no_404=true&referer=https%3A%2F%2Frutube.ru`;
+				fetch(pls)
+					.then(res => res.json())
+					.then((json) => {
+						// Получаем название ролика
+						const outputTitle = json.title;
+						// Получаем информацию о плейлистах
+						let video_m3u8 = json['video_balancer']['m3u8'];
+						fetch(video_m3u8)
+							.then(res => res.text())
+							.then(text => {
+								let parser = new m3u8Parser.Parser();
+								parser.push(text);
+								parser.end();
+								let parsedManifest = parser.manifest;
+								// Получаем ссылку на плейлист самого большого разрешения видео
+								let m3u8 = parsedManifest['playlists'][parsedManifest['playlists'].length -1]['uri'];
+								// Получаем ссылку для составления в будующем ссылки на сегмент
+								const myURL = URL.parse(m3u8);
+								let pathname = myURL.pathname.split("/");
+								pathname.pop();
+								const urlPrefix = myURL.protocol + "//" + myURL.host + "/" + pathname.join("/") + "/";
+								// Получаем плейлист с сегментами
+								fetch(m3u8)
+									.then(res => res.text())
+									.then(async text => {
+										await createDir(__dirname + "/video");
+										await deleteFiles(/^segment-.*\.ts/, __dirname + '/video');
+										console.log(" ");
+										console.log("DOWNLOAD:", outputTitle, "\n");
+
+										const progress = new cliProgress.SingleBar({
+											stopOnComplete: true,
+											hideCursor: false,
+											autopadding: true,
+											fps: 5,
+											barsize: 37
+										},{
+											format: formatBar,
+											barCompleteChar: '\u2588',
+											barIncompleteChar: '\u2592'
+										});
+										let m3u8Video = new m3u8Parser.Parser();
+										m3u8Video.push(text);
+										m3u8Video.end();
+										// Сегменты
+										let segments = m3u8Video.manifest.segments;
+										let key, data = "", int;
+										// Получаем ссылки сегментов
+										let arrFiles = [];
+										// Забрать расширение сегмента
+										let ext;
+										progress.start(segments.length, 0, {filename: " "});
+
+										for (key in segments) {
+											// Забираем расширение
+											ext = path.extname(segments[key]['uri']);
+											// По сути здесь можно качать и сохранять
+											int = parseInt(key) + 1;
+											let fname = 'segment-' + `${int}`.padStart(10, '0') + ext;
+											try {
+												let rs = await fetch(urlPrefix + segments[key]['uri']);
+												if(rs.ok){
+													const f = __dirname + "/video/" + fname;
+													arrFiles.push(f);
+													progress.update(int, {filename: "   SAVE: " + _colors.yellowBright(fname)});
+													await streamPipeline(rs.body, fs.createWriteStream(f));
+												}else{
+													progress.update(int, {filename: "NO SAVE: " + _colors.redBright(fname)});
+													progress.stop();
+													console.error(_colors.redBright("ПРОВЕРЬТЕ ПОДКЛЮЧЕНИЕ К ИНТЕРНЕТУ"));
+													process.exit(1);
+												}
+											}catch(e){
+												progress.update(int, {filename: "NO SAVE: " + _colors.redBright(fname)});
+												progress.stop();
+												console.error(_colors.redBright("ПРОВЕРЬТЕ ПОДКЛЮЧЕНИЕ К ИНТЕРНЕТУ"));
+												process.exit(1);
+											}
+											await delay(50);
+										}
+										progress.update(int, {filename: " "});
+										await delay(1000);
+										progress.stop();
+										const saveTitle = sanitize(outputTitle);
+										console.log("\u00A0");
+										console.log("COMBINING FILES:", _colors.yellowBright(`${arrFiles.length}`), "FILES INTO A", _colors.yellowBright(`"${saveTitle}${ext}"`), "PLEASE WAIT...", "\n");
+										await splitFile.mergeFiles(arrFiles, `${__dirname}/video/${saveTitle}${ext}`);
+										console.log("\u00A0");
+										console.log("DELETE FILES:", _colors.yellowBright(`${arrFiles.length}`), "\n");
+										await deleteFiles(/^segment-.*\.ts/, __dirname + '/video');
+										await deleteFile(`${__dirname}/video/${saveTitle}.mp4`);
+										console.log("\u00A0");
+										console.log("CONVERTING", _colors.yellowBright(`"${saveTitle}${ext}"`), "TO", _colors.yellowBright(`"${saveTitle}.mp4"`),"PLEASE WAIT...");
+										console.log("\u00A0");
+										await execFFmpeg(`${__dirname}/video/${saveTitle}${ext}`, `${__dirname}/video/${saveTitle}.mp4`);
+										console.log(_colors.yellowBright("DONE!"));
+										console.log("\u00A0");
+										console.log(_colors.bgWhite(_colors.white("█████████████████")) + "\u00A0");
+										console.log(_colors.bgRed(_colors.white(" #СвоихНеБросаем ")) + "\u00A0");
+										console.log(_colors.bgBlue(_colors.blue("█████████████████")) + "\u00A0");
+										console.log("\u00A0");
+										resolve(true);
+									})
+							});
+					});
+			}else {
+				reject(true);
+			}
+		});
+	};
+
+(async function(){
+	if (urls.length) {
+		for(let i = 0; i < urls.length; ++i){
+			url = urls[i];
+			await runDownLoadAndConverting(url)
+		}
+	}
+}());
